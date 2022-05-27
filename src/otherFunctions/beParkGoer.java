69d11fc13d0fe503cd75203714264ee2d91af302
AerialAstrea <elizabethtdau@gmail.com>
2022-05-26 20:28:17 -0700

    beParkGoer modified

--- src/otherFunctions/beParkGoer.java
+++ src/otherFunctions/beParkGoer.java
@@ -1,5 +1,3 @@
-package src.otherFunctions;
-
 import java.util.*;
 import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
@@ -12,8 +10,8 @@ public class beParkGoer {
         
         ImageIcon my_icon = new ImageIcon("./Images/disney_world_logo.png");
 
-        String [] btn_options = {"See Attraction List", "Purchase a Ticket", "Leave"};
-        String [] btn_options2 = {"See Attraction List", "Check Ticket", "Leave"};
+        String [] btn_options = {"Leave","Purchase a Ticket","See Attraction List"};
+        String [] btn_options2 = {"Leave","Check Ticket","See Attraction List"};
         String [] yn_options = {"Yes!", "No"};
         String [] be_options = {"Main Menu", "Back"};
         String [] attractions = {"Back","Basic Buggy","Relatively Extrema","Coaster Toaster"};
@@ -31,7 +29,97 @@ public class beParkGoer {
                 result = JOptionPane.showOptionDialog(null, "What would you like to do?", "Main Menu",
                 JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, my_icon, btn_options, 2);
                 
-            if(result == 0){//user selected see attraction list
+            if(result == 0){//leave option
+                JOptionPane.showMessageDialog(null, "Thank you for visiting!", "DisneyLand",JOptionPane.INFORMATION_MESSAGE,my_icon);
+                leave = true;
+            }
+            else if(result == 1){//purchase a ticket option
+                int partySize = Integer.valueOf(JOptionPane.showInputDialog("How many people are in your party?"));
+                ArrayList<Rider> riders = new ArrayList<Rider>();
+                for(int i = 0; i < partySize; i++){
+                    JOptionPane.showMessageDialog(null,"Enter Party Member " + (i+1) + " Information", "DisneyLand", JOptionPane.PLAIN_MESSAGE);
+                    String name = JOptionPane.showInputDialog("Name:");
+                    int height = Integer.valueOf(JOptionPane.showInputDialog("Height(in inches):"));
+                }
+                result = JOptionPane.showOptionDialog(null, "Would you like to get a fast pass?", null, JOptionPane.YES_NO_OPTION,
+                JOptionPane.INFORMATION_MESSAGE,my_icon, yn_options,1);
+                boolean hasFP = false;
+                if(result == 0)
+                hasFP = true;
+                double balance = Double.valueOf(JOptionPane.showInputDialog("Please enter your starting balance"));
+                
+                Ticket t = new Ticket(riders,hasFP,balance);
+                t.printParty();
+                while(!leave){
+                    result = JOptionPane.showOptionDialog(null, "What would you like to do next?", "Main Menu",
+                    JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, my_icon, btn_options2, 2);
+                    if(result == 0){//see attraction option
+                        JOptionPane.showMessageDialog(null, "Thank you for visiting!", "DisneyLand",JOptionPane.INFORMATION_MESSAGE,my_icon);
+                        leave = true;
+                    }
+                    else if(result == 1){//check ticket option
+                        boolean looking = true;
+                        
+                        while(looking){
+                            String [] options = {"Back","Deposit","Withdraw"};
+                            if(!t.hasFastPass()){
+                                String [] temp = {"Back","Deposit","Withdraw", "Get Fast Pass"};
+                                options = temp;
+                            }
+                            
+                            result = JOptionPane.showOptionDialog(null, "What would you like to do?", "DisneyWorld",
+                            JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, my_icon, options, 0);
+                            
+                            if(result == 0)
+                                looking = false;
+                            else if(result == 1){//Deposit option
+                                double value = Double.valueOf(JOptionPane.showInputDialog("How much would you like to deposit?"));
+                                t.deposit(value);
+                            }
+                            else if(result == 2){//Withdraw option
+                                double value = Double.valueOf(JOptionPane.showInputDialog("How much would you like to deposit?"));
+                                t.withdraw(value);
+                            }
+                            else if(result == 3){//Get Fast Pass option
+                                t.getFastPass();
+                            }
+                        }
+                    }
+                    else{//see attraction option
+                        boolean looking = true; //true = still looking at attractions
+                
+                        while(looking){
+                            result = JOptionPane.showOptionDialog(null, "Here is a list of our attractions!", "DisneyWorld",
+                            JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, my_icon, attractions, 0);
+                            
+                            if(result == 0)
+                                looking = false;
+                            else if(result == 1){//Basic Buggy
+                                result = JOptionPane.showOptionDialog(null, "Basic Buggy\n" + basicBuggy.getInfo(), null, JOptionPane.YES_NO_OPTION,
+                                JOptionPane.INFORMATION_MESSAGE,my_icon, be_options,1);
+                                if(result == 0){
+                                    looking = false;
+                                }
+                            }
+                            else if(result == 2){//Relatively Extrema
+                                result = JOptionPane.showOptionDialog(null, "Relatively Extrema\n" + relativelyExtrema.getInfo(), null, JOptionPane.YES_NO_OPTION,
+                                JOptionPane.INFORMATION_MESSAGE,my_icon, be_options,1);
+                                if(result == 0){
+                                    looking = false;
+                                }
+                            }
+                            else if(result == 3){//Coaster Toaster
+                                result = JOptionPane.showOptionDialog(null, "Coaster Toaster\n" + coasterToaster.getInfo(), null, JOptionPane.YES_NO_OPTION,
+                                JOptionPane.INFORMATION_MESSAGE,my_icon, be_options,1);
+                                if(result == 0){
+                                    looking = false;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            else if(result == 2){//user selected leave
                 boolean looking = true; //true = still looking at attractions
                 
                 while(looking){
@@ -63,90 +151,6 @@ public class beParkGoer {
                     }
                 }
             }
-            else if(result == 1){//user selected purchase/check a ticket
-                int partySize = Integer.valueOf(JOptionPane.showInputDialog("How many people are in your party?"));
-                ArrayList<Rider> riders = new ArrayList<Rider>();
-                for(int i = 0; i < partySize; i++){
-                    JOptionPane.showMessageDialog(null,"Enter Party Member " + (i+1) + " Information", "DisneyLand", JOptionPane.PLAIN_MESSAGE);
-                    String name = JOptionPane.showInputDialog("Name:");
-                    int height = Integer.valueOf(JOptionPane.showInputDialog("Height(in inches):"));
-                }
-                result = JOptionPane.showOptionDialog(null, "Would you like to get a fast pass?", null, JOptionPane.YES_NO_OPTION,
-                JOptionPane.INFORMATION_MESSAGE,my_icon, yn_options,1);
-                boolean hasFP = false;
-                if(result == 0)
-                hasFP = true;
-                double balance = Double.valueOf(JOptionPane.showInputDialog("Please enter your starting balance"));
-                
-                Ticket t = new Ticket(riders,hasFP,balance);
-                t.printParty();
-                while(!leave){
-                    result = JOptionPane.showOptionDialog(null, "What would you like to do next?", "Main Menu",
-                    JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, my_icon, btn_options2, 2);
-                    if(result == 0){//see attraction option
-                        boolean looking = true; //true = still looking at attractions
-                
-                        while(looking){
-                            result = JOptionPane.showOptionDialog(null, "Here is a list of our attractions!", "DisneyWorld",
-                            JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, my_icon, attractions, 0);
-                            
-                            if(result == 0)
-                                looking = false;
-                            else if(result == 1){//Basic Buggy
-                                result = JOptionPane.showOptionDialog(null, "Basic Buggy\n" + basicBuggy.getInfo(), null, JOptionPane.YES_NO_OPTION,
-                                JOptionPane.INFORMATION_MESSAGE,my_icon, be_options,1);
-                            }
-                            else if(result == 2){//Relatively Extrema
-                                result = JOptionPane.showOptionDialog(null, "Relatively Extrema\n" + relativelyExtrema.getInfo(), null, JOptionPane.YES_NO_OPTION,
-                                JOptionPane.INFORMATION_MESSAGE,my_icon, be_options,1);
-                            }
-                            else if(result == 3){//Coaster Toaster
-                                result = JOptionPane.showOptionDialog(null, "Coaster Toaster\n" + coasterToaster.getInfo(), null, JOptionPane.YES_NO_OPTION,
-                                JOptionPane.INFORMATION_MESSAGE,my_icon, be_options,1);
-                            }
-                        }
-                    }
-                    else if(result == 1){//check ticket option
-                        boolean looking = true;
-                        
-                        while(looking){
-                            String [] options = {"Back","Deposit","Withdraw"};
-                            if(!t.hasFastPass()){
-                                String [] temp = {"Back","Deposit","Withdraw", "Get Fast Pass"};
-                                options = temp;
-                            }
-                            
-                            result = JOptionPane.showOptionDialog(null, "What would you like to do?", "DisneyWorld",
-                            JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.INFORMATION_MESSAGE, my_icon, options, 0);
-                            
-                            if(result == 0)
-                                looking = false;
-                            else if(result == 1){//Deposit option
-                                double value = Double.valueOf(JOptionPane.showInputDialog("How much would you like to deposit?"));
-                                t.deposit(value);
-                            }
-                            else if(result == 2){//Withdraw option
-                                double value = Double.valueOf(JOptionPane.showInputDialog("How much would you like to deposit?"));
-                                t.withdraw(value);
-                            }
-                            else if(result == 3){//Get Fast Pass option
-                                t.getFastPass();
-                            }
-                        }
-                    }
-                    else{//leave option
-                        JOptionPane.showMessageDialog(null, "Thank you for visiting!", "DisneyLand",JOptionPane.INFORMATION_MESSAGE,my_icon);
-                        leave = true;
-                    }
-                }
-            }
-            else if(result == 2){//user selected leave
-                JOptionPane.showMessageDialog(null, "Thank you for visiting!", "DisneyLand",JOptionPane.INFORMATION_MESSAGE,my_icon);
-                leave = true;
-            }
         }
-    }
-    
-    
+    }  
 }
-
